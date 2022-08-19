e631d4e7876171fd6424caeef3cf5b19bb712b37
Alam Wibowo <admhabits@gmail.com>
2022-08-19 13:58:53 +0700

    Add Login UI

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,6 +1,33 @@
+import Styles from '../../styles/Home.module.css';
 
-export default function register() {
+export default function Register() {
+
+  const handleOnclick = (e: any) => {
+    e.preventDefault();
+    console.log('hi');
+  }
   return (
-    <div>register</div>
+    <div className={Styles.container}>
+        <main>
+            <div className="card p-2" style={{ width: "18rem;" }}>
+                <div className="card-body bg-dark rounded">
+                    <h5 className="card-title text-center mb-3 text-white">Register</h5>
+                    <form className='mt-3'>
+                    <div className="mb-2">
+                        <label htmlFor="exampleInputEmail1" className="form-label text-white">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="exampleInputPassword1" className="form-label text-white">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                  
+                    <button onClick={handleOnclick} className="btn btn-sm btn-primary mt-3" style={{ float: 'right' }}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </main>
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <button type="button" className="btn btn-primary">Primary</button>
+    <div className="home">
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+      </ul>  
     </div>
   )
 }
