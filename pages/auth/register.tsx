81fe59d041f3d6b51f7a1ac28ae5ecfa42e423ca
Alam Wibowo <admhabits@gmail.com>
2022-08-19 15:08:42 +0700

    Add Register Page

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
+import { useState } from 'react';
 import Styles from '../../styles/Home.module.css';
 
 export default function Register() {
+    const [error, setError] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
 
   const handleOnclick = (e: any) => {
     e.preventDefault();
-    console.log('hi');
+    axios.post('https://reqres.in/api/login', {email, password: pass})
+    .then(res => {
+        console.log(res.data);
+        window.location.href = "/auth/login";
+    })
+    .catch(error => {
+        console.log(error.response.data.error);
+        setError(error.response.data.error);
+    })
   }
   return (
     <div className={Styles.container}>
@@ -15,12 +28,19 @@ export default function Register() {
                     <form className='mt-3'>
                     <div className="mb-2">
                         <label htmlFor="exampleInputEmail1" className="form-label text-white">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        {
+                            !error && error ==="" ? (
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            ): (
+                        <div id="emailHelp" className="form-text">{error}</div>
+                                
+                            )
+                        }
                     </div>
                     <div className="mb-2">
                         <label htmlFor="exampleInputPassword1" className="form-label text-white">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1"/>
+                        <input type="password" value={pass} onChange={(e) => setPass(e.target.value)} className="form-control" id="exampleInputPassword1"/>
                     </div>
                   
                     <button onClick={handleOnclick} className="btn btn-sm btn-primary mt-3" style={{ float: 'right' }}>Submit</button>
