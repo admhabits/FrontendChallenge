9d814b2a14507d5bef05fbadd2a5a716934c522e
Alam Wibowo <admhabits@gmail.com>
2022-08-20 17:08:04 +0700

    Get Detail Blogs

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,10 +1,49 @@
-import React from 'react'
 import Header from '../../components/layouts/Header'
+import { database } from '../../services/FirebaseConfig';
+import { collection, doc, getDoc } from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+
 
 export default function DetailBlogs() {
+  const [detailBlog, setDetailBlog] = useState(null);
+  const router = useRouter();
+  const getDetailBlogs = async () => {
+    const { id } = router.query;
+    const docRef = doc(database, 'blogs', id);
+    // console.log(id);
+    try {
+        const docSnap = await getDoc(docRef);
+        if(docSnap.exists()){
+          console.log(docSnap.data());
+          setDetailBlog(docSnap.data());
+        } else {
+          alert('Tidak ditemukan data!');
+        }
+    } catch (error) {
+      alert(error)
+    }
+  }
+  useEffect(() => {
+    getDetailBlogs();
+  }, [])
+  
   return (
     <div className='container mt-2'>
-      <Header title="Detail Postingan"/>
+      {
+        !detailBlog ? (
+          <div className='row'>
+            <div className='col-12'>
+              <p className='text-primary text-center'>Loading...</p>
+            </div>
+          </div>
+        ) : (
+         <>
+           <Header title={detailBlog?.title}/>
+          <p>{detailBlog?.desc}</p>
+         </>
+        )
+      }
     </div>
   )
 }
