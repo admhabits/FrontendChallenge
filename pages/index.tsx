380503b7ad573115cc41ab384f75afff00edeacd
Alam Wibowo <admhabits@gmail.com>
2022-08-20 12:46:50 +0700

    Add Post UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,70 @@ export default function Home() {
     <div className="container mt-2">
       <Header title="Semua Konten"/>
       <div className="row">
-        <div className="col-md-6 col-sm-12 col-lg-4 mb-2">
+        <div className="col-md-6 col-sm-12 col-lg-4 mb-lg-4 mb-md-3 mb-sm-2">
+            <div className="card">
+              <div className="card-header">Judul Postingan</div>
+              <div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, molestias.</div>
+              <div className="card-footer d-flex justify-content-end gap-2">
+                <button className="btn btn-sm btn-danger">Remove</button>
+                <button className="btn btn-sm btn-warning">Edit</button>
+                <button className="btn btn-sm btn-primary">Read More</button>
+              </div>
+            </div>
+        </div>
+        <div className="col-md-6 col-sm-12 col-lg-4 mb-lg-4 mb-md-3 mb-sm-2">
           <div className="card">
             <div className="card-header">Judul Postingan</div>
             <div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, molestias.</div>
+            <div className="card-footer d-flex justify-content-end gap-2">
+              <button className="btn btn-sm btn-danger">Remove</button>
+              <button className="btn btn-sm btn-warning">Edit</button>
+              <button className="btn btn-sm btn-primary">Read More</button>
+            </div>
           </div>
         </div>
-        
-        <div className="col-md-6 col-sm-12 col-lg-4 mb-3">
+        <div className="col-md-6 col-sm-12 col-lg-4 mb-lg-4 mb-md-3 mb-sm-2">
           <div className="card">
             <div className="card-header">Judul Postingan</div>
             <div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, molestias.</div>
+            <div className="card-footer d-flex justify-content-end gap-2">
+              <button className="btn btn-sm btn-danger">Remove</button>
+              <button className="btn btn-sm btn-warning">Edit</button>
+              <button className="btn btn-sm btn-primary">Read More</button>
+            </div>
           </div>
         </div>
-        <div className="col-md-6 col-sm-12 col-lg-4 mb-3">
+        <div className="col-md-6 col-sm-12 col-lg-4 mb-lg-4 mb-md-3 mb-sm-2">
           <div className="card">
             <div className="card-header">Judul Postingan</div>
             <div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, molestias.</div>
+            <div className="card-footer d-flex justify-content-end gap-2">
+              <button className="btn btn-sm btn-danger">Remove</button>
+              <button className="btn btn-sm btn-warning">Edit</button>
+              <button className="btn btn-sm btn-primary">Read More</button>
+            </div>
           </div>
         </div>
-        <div className="col-md-6 col-sm-12 col-lg-4 mb-3">
+        <div className="col-md-6 col-sm-12 col-lg-4 mb-lg-4 mb-md-3 mb-sm-2">
           <div className="card">
             <div className="card-header">Judul Postingan</div>
             <div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, molestias.</div>
+            <div className="card-footer d-flex justify-content-end gap-2">
+              <button className="btn btn-sm btn-danger">Remove</button>
+              <button className="btn btn-sm btn-warning">Edit</button>
+              <button className="btn btn-sm btn-primary">Read More</button>
+            </div>
           </div>
         </div>
-        <div className="col-md-6 col-sm-12 col-lg-4 mb-3">
+        <div className="col-md-6 col-sm-12 col-lg-4 mb-lg-4 mb-md-3 mb-sm-2">
           <div className="card">
             <div className="card-header">Judul Postingan</div>
             <div className="card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, molestias.</div>
+            <div className="card-footer d-flex justify-content-end gap-2">
+              <button className="btn btn-sm btn-danger">Remove</button>
+              <button className="btn btn-sm btn-warning">Edit</button>
+              <button className="btn btn-sm btn-primary">Read More</button>
+            </div>
           </div>
         </div>
       </div>
