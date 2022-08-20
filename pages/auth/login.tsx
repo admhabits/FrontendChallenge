ea8b248f5bce8b0397d07e8508e32ef6e3e120d1
Alam Wibowo <admhabits@gmail.com>
2022-08-20 19:01:15 +0700

    Update UI Login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -22,12 +22,15 @@ export default function login() {
   return (
     <div className={Styles.container}>
         <main className='bg-green'>
-            <div className="card p-2" style={{ width: "18rem;" }}>
-                <div className="card-body bg-dark rounded">
-                    <h5 className="card-title text-center mb-3 text-white">Login</h5>
+            <div className="card p-1 bg-primary" style={{ width: "18rem;" }}>
+                <div className='card-header bg-white'>
+                <h5 className="card-title text-center text-primary">Login</h5>
+                </div>
+                <div className="card-body bg-white rounded mt-2">
+                   
                     <form className='mt-3'>
                     <div className="mb-2">
-                        <label htmlFor="exampleInputEmail1" className="form-label text-white">Email address</label>
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                         <input type="email" className="form-control" value={email} onChange={ (e) => setEmail(e.target.value)}  id="exampleInputEmail1" aria-describedby="emailHelp"/>
                         {
                             error !== "" && (
@@ -35,13 +38,16 @@ export default function login() {
                             )
                         }
                     </div>
-                    <div className="mb-2">
-                        <label htmlFor="exampleInputPassword1" className="form-label text-white">Password</label>
+                    <div className="mb-1">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                         <input type="password" value={pass} onChange={(e)=> setPass(e.target.value)} className="form-control" id="exampleInputPassword1"/>
                     </div>
                   
-                    <button onClick={handleOnclick} className="btn btn-sm btn-primary mt-3" style={{ float: 'right' }}>Submit</button>
+                  
                     </form>
+                </div>
+                <div className='card-footer mt-2 bg-white shadow'>
+                <button onClick={handleOnclick} className="btn btn-sm btn-primary" style={{ float: 'right' }}>Submit</button>
                 </div>
             </div>
         </main>
